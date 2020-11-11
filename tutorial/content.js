c9b7f6135b8bd503f3d0768d4a1b6b236c8d58d2
Stine <stine@sandornes.com>
2020-11-11 09:42:13 -0500

    through p217

--- tutorial/content.js
+++ tutorial/content.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("Hello World!");
+var pageTitle = $("title").text();
+console.log("The title of the page is " + pageTitle);
+
+// get scripts on the page
+var scriptsFound = document.getElementsByTagName("script");
+// store trackers we find
+var trackersFound = [];
+// loop through scripts
+for (var i = 0, l = scriptsFound.length; i < l; i++) {
+  // if script tag has a src attribute
+  if (scriptsFound[i].src !== "") {
+    // get root domain of scripts src
+    var scriptDomain = extractRootDomain(scriptsFound[i].src);
+    // look to see if tracker root domain is in disconnectTrackingServices
+    if (disconnectTrackingServices.indexOf(scriptDomain) >= 0) {
+      console.log("👀👀getTrackers()", scriptDomain);
+      // add to found list
+      trackersFound.push(scriptDomain);
+    }
+  }
+}
